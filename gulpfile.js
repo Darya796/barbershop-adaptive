672b50f5cf6fe342f7b22d71ae49d92782e49cd6
Darya <dashutkadevi@gmail.com>
2018-01-17 18:40:25 +0200

    finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require("gulp");
-var less = require("gulp-less");
+// var rename = require("gulp-rename");
 var plumber = require("gulp-plumber");
+var less = require("gulp-less");
+// var minify = require("gulp-csso");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+// var mqpacker = require("css-mqpacker");
+// var imagemin = require("gulp-imagemin");
 var server = require("browser-sync");
+// var run = require("run-sequence");
+// var del = require("del");
+
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -17,15 +24,53 @@ gulp.task("style", function() {
       "last 2 Opera versions",
       "last 2 Edge versions"
     ]})
+    // ,
+    // mqpacker({
+    //   sort: true
+    // })
   ]))
   .pipe(gulp.dest("css"))
+  // .pipe(minify())
+  // .pipe(rename("style.min.css"))
+  // .pipe(gulp.dest("css"))
   .pipe(server.reload({stream: true}));
 });
+
+// gulp.task("images", function() {
+//   return gulp.src("img/**/*.{png,jpg,gif}")
+//   .pipe(imagemin([
+//     imagemin.optipng({optimizationLevel: 3}),
+//     imagemin.jpegtran({progressive: true})
+//   ]))
+//   .pipe(gulp.dest("img"));
+// });
+
 gulp.task("start", ["style"], function() {
   server.init({
     server: "."
   });
+
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html")
   .on("change", server.reload);
 });
+
+// gulp.task("build", function(fn) {
+//   run("style", "images", fn);
+// });
+
+// gulp.task("copy", function() {
+//   return gulp.src([
+//     "fonts/**/*.{woff,woff2}",
+//     "img/**",
+//     "js/**",
+//     "*.html"
+//   ], {
+//     base: "."
+//   })
+//   .pipe(gulp.dest("build"));
+// });
+
+// gulp.task("clean", function() {
+//   return del("build");
+// });
--- js/script.js
+++ js/script.js
@@ -12,3 +12,60 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+
+var link = document.querySelector(".login-form-link");
+var popup = document.querySelector(".login-form");
+var popup_wrapper = popup.querySelector(".login-form__wrapper");
+var close = popup.querySelector(".login-form__btn-close");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("login");
+
+var overlay = document.querySelector(".overlay");
+
+
+link.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.add("login-form-show");
+  popup_wrapper.classList.add("login-form__wrapper-show");
+  navMain.classList.add("main-nav--closed");
+  navMain.classList.remove("main-nav--opened");
+  overlay.classList.add("overlay-show");
+  login.focus();
+
+  if (storage) {
+    login.value = storage;
+    password.focus();
+      overlay.classList.add("overlay-show");
+  }
+});
+
+
+close.addEventListener("click", function(event){
+  event.preventDefault();
+  popup.classList.remove("login-form-show");
+  overlay.classList.remove("overlay-show");
+});
+
+
+form.addEventListener("submit", function (event) {
+ if (!login.value || !password.value){
+ event.preventDefault();
+} else {
+ localStorage.setItem("login", login.value);
+}
+});
+
+window.addEventListener("keydown", function (event) {
+ if (event.keyCode === 27) {
+   if (popup.classList.contains("login-form-show")) {
+     popup.classList.remove("login-form-show");
+     overlay.classList.remove("overlay-show");
+   }
+ }
+});
+
+// var html = document.documentElement;
+// html.className = html.className.replace("no-js", "js");
